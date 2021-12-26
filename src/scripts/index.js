85117ba01ce6c986d5c3141e4fa55a6c544a6522
Artem Rachkov <ateee@yandex.ru>
2021-12-26 12:11:02 +0300

    баг с vh в мобильной версии

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,6 +3,9 @@ import '../index.html';
 import '../styles/index.scss';
 import '../vendor/materialize.min.js';
 
+let vh = window.innerHeight * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 document.addEventListener('DOMContentLoaded', function(){
 
 
